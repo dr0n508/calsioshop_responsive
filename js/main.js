27fc10369e7bf6aaf7b4152d14af13adfb11d7d8
kornienko.andriy <kornienko.andriy@gmail.com>
2017-02-18 16:54:57 +0200

    add categories

--- js/main.js
+++ js/main.js
@@ -10,6 +10,21 @@ $(document).ready(function () {
         // $(".list-group-item, .list-group-2-level").addClass("in");
     });
 
+    maintainSameHeight($('[data-same-height="category"]'));
+
+    function maintainSameHeight($list) {
+        var height = 0;
+
+        $list.each(function () {
+            var $this = $(this);
+            if ($this.outerHeight() > height) {
+                height = $this.outerHeight();
+            }
+        });
+
+        $list.css("height", height);
+    }
+
 
 
 
