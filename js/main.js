e2af910be6ef600ce07154c7e434d870a26f1705
kornienko.andriy <kornienko.andriy@gmail.com>
2017-02-18 15:45:50 +0200

    add slider home page

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 $(document).ready(function () {
+    $('.home-page-slider').bxSlider({
+        infiniteLoop: false,
+        hideControlOnEnd: true,
+        pager: false
+    });
 
 
     $( "#ddd" ).click(function() {
-
-
-
         // $(".list-group-item, .list-group-2-level").addClass("in");
-
-
     });
+
+
 
 
 });
