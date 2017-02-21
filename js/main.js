b93bee081fa7161ca8cb5d2508a6f35364cefc8c
kornienko.andriy <kornienko.andriy@gmail.com>
2017-02-21 21:41:57 +0200

    dd

--- js/main.js
+++ js/main.js
@@ -53,4 +53,49 @@ $(document).ready(function () {
         templateSelection: formatState,
         templateResult: formatState
     });
+
+
+
+    /**
+     * jQuery UI slider widget
+     * Drag a handle to select a numeric value.
+     */
+    $("#slider").slider({
+        min: 0,
+        max: 1000,
+        values: [0, 1000],
+        range: true,
+        stop: function(event, ui) {
+            $("input#minCost").val($("#slider").slider("values",0));
+            $("input#maxCost").val($("#slider").slider("values",1));
+        },
+        slide: function(event, ui){
+            $("input#minCost").val($("#slider").slider("values",0));
+            $("input#maxCost").val($("#slider").slider("values",1));
+        }
+    });
+
+    $("input#minCost").change(function(){
+        var value1=$("input#minCost").val();
+        var value2=$("input#maxCost").val();
+
+        if(parseInt(value1) > parseInt(value2)){
+            value1 = value2;
+            $("input#minCost").val(value1);
+        }
+        $("#slider").slider("values",0,value1);
+    });
+
+    $("input#maxCost").change(function(){
+        var value1=$("input#minCost").val();
+        var value2=$("input#maxCost").val();
+
+        if (value2 > 1000) { value2 = 1000; $("input#maxCost").val(1000)}
+
+        if(parseInt(value1) > parseInt(value2)){
+            value2 = value1;
+            $("input#maxCost").val(value2);
+        }
+        $("#slider").slider("values",1,value2);
+    });
 });
